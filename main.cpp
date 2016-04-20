b4774a5dc86f76b37ad632250597d1f19274ba10
Kenneth B <kmbolando@eths202.org>
2016-04-20 15:24:00 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+/* itt delete this
+
+At the end of my lesson,
+I will input all my knowledge into this one C++ file
+and hope it will do something
+
+or create a repository called "learncpp final" for a bigger multi-file project
+
+itt create pixels
+*/
